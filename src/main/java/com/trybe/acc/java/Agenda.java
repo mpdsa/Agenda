b9eab1e8db3dd84e1120820593b86d3007e2b659
marcos <marcos@li0001>
2022-09-08 19:14:25 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/Agenda.java
+++ src/main/java/com/trybe/acc/java/Agenda.java
@@ -3,8 +3,25 @@ package com.trybe.acc.java;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Classe Agenda. */
 public class Agenda {
-	private List<String> contatos = new ArrayList<>();
+  private List<String> contatos = new ArrayList<>();
 	
+  void incluirContato(String pessoa) {
+    contatos.add(pessoa);
+    
+  }
+  
+  void excluirContato(String pessoa) {
+    contatos.remove(pessoa);
+  }
+  
+  boolean verificaContato(String pessoa) {
+    return contatos.contains(pessoa);
+  }
+  
+  int tamanhoAgenda() {
+    return contatos.size();
+  }
 	
 }
